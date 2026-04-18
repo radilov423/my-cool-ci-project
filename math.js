daa0bd9a497b2a6e546103290f39aad95bfa7eff
radilov423 <radilov2019@gmail.com>
2026-04-18 23:46:01 +0300

    feet: fix add function and add dayjs support

--- math.js
+++ math.js
@@ -1,3 +1,9 @@
+import dayjs from "dayjs";
+
 export function add(a, b) {
-    return a - b;
+  return a + b;
+}
+
+export function getFormattedDate() {
+  return dayjs().format('YYYY-MM-DD');
 }
--- math.test.js
+++ math.test.js
@@ -1,6 +1,12 @@
 import { expect, test } from 'vitest'
-import { add } from './math'
+import { add, getFormattedDate } from './math'
 
 test('adds 1 + 2 to equal 3', () => {
     expect(add(1, 2)).toBe(3)
+})
+
+test('return date in correct format', () => {
+    const date = getFormattedDate();
+
+    expect(date).toMatch(/^\d{4}-\d{2}-\d{2}$/)
 })
